51652467b48d2769bb1d16822c2322ed36dba5b5
Carlo <onesilop@users.noreply.github.com>
2020-02-09 16:58:56 +1100

    intitial Commit

---
+++ territory.mobile/territory.mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using territory.mobile.Models;
+
+namespace territory.mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
